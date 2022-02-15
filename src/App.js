5cd207aaa17f468d446eea16d0d56dd3adaa1453
Claire Rhoda <clairerhoda@Claires-MacBook-Pro.local>
2022-02-14 17:48:57 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var showWelcome = true;
+  const touchDetected = () => {
+    if (showWelcome) {
+      showWelcome = false;
+      document.querySelector('#welcome-screen').style.display = "none";
+      document.querySelector('#home-screen').style.display = "flex";
+    }
+  }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="register">
+        <div onClick={() => {touchDetected();}} id="interactive-screen">
+          <div id="welcome-screen"><div id="title1">Welcome</div><div id="title2">Touch to Begin</div></div>
+          <div id="home-screen">
+            <div id="total">Total</div>
+            <div id="number-pad">
+              <div onClick={() => null} className="number-btn">1</div>
+              <div onClick={() => null} className="number-btn">2</div>
+              <div onClick={() => null} className="number-btn">3</div>
+              <div onClick={() => null} className="number-btn">4</div>
+              <div onClick={() => null} className="number-btn">5</div>
+              <div onClick={() => null} className="number-btn">6</div>
+              <div onClick={() => null} className="number-btn">7</div>
+              <div onClick={() => null} className="number-btn">8</div>
+              <div onClick={() => null} className="number-btn">9</div>
+              <div onClick={() => null} className="number-btn">0</div>
+            </div>
+            <div id="look-up-btn">Look Up Item</div>
+            <div id="finish-btn">$ Finish and Pay</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
