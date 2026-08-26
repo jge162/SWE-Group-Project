17eae0f693bdb55ae462b22564e4995f55411c27
jeremyge <31228460+jge162@users.noreply.github.com>
2026-08-26 16:45:38 -0700

    Update SearchList.js
    Removed unused states

--- src/SearchList.js
+++ src/SearchList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import './css/lookUpScreen.css';
 import groceryUtils from './grocery';
 function SearchList({ filteredItems }) {
